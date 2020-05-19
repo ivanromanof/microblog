d70c90e8ef6f519e70ed1db6c07c06c4015cb74c
ivanromanof <ivanromanov123@yandex.ru>
2020-05-19 20:21:32 +0300

    added templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello My App!"
+    user = {'username': 'Ivan'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        {
+            'author': {'username': 'Bob'},
+            'body': 'Bla bla bla!'
+        },
+        {
+            'author': {'username': 'Sussan'},
+            'body': 'The Advengers movies was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
